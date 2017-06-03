4c0f61fa51bb5a3812b262a6495a4cff96982fae
Laurent Mignot <laurent@mignot.eu>
2017-06-03 11:48:18 +0100

    for comprehension version of duel

--- src/main/scala/exercises/Dwarves.scala
+++ src/main/scala/exercises/Dwarves.scala
@@ -7,8 +7,7 @@ object Dwarves extends App {
   type Duel = (String, String)
 
   println(duels(List("Thorin", "Gloin", "Balin")))
-
-//  println(List("Thorin", "Gloin", "Balin").combinations(2).toList)
+  println(duelsFC(List("Thorin", "Gloin", "Balin")))
 
   def duels(dwarves: List[String]): List[Duel] = {
     dwarves
@@ -16,4 +15,13 @@ object Dwarves extends App {
       .filter(a => a._1 != a._2)
   }
 
+  def duelsFC(dwarves: List[String]): List[Duel] = {
+    for {
+      x <- dwarves
+      z <- for (y <- dwarves) yield (x, y)
+      if z._1 != z._2
+    } yield z
+  }
+
+  def duelsRec(dwarves: List[String]): List[Duel] = ???
 }
